89ae2d3473c84e3cfbc325e1ebd0080d66193fab
Ursulo Cabeza <ursuloacabezad@gmail.com>
2022-11-07 19:58:43 -0400

    agregando leccion-2

---
+++ modulo-1/leccion-2/variables.js
@@ -0,0 +1,16 @@
+// VARIABLES
+
+const estudiante = "Ursulo";
+
+// VAR - LET - CONST
+
+var edad = 20;
+
+let pais = "Venezuela";
+
+// DIFERENCIA - LET Y CONST
+// LET PERMITE REASIGNAR EL VALOR Y CONST NO!!!
+let mascota = "perro";
+mascota = "gato";
+
+const animal = "pajaro";
