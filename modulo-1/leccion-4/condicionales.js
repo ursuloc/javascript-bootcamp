90f033e0ebfe1115824c7ef9c7d8101b303caca4
Ursulo Cabeza <ursuloacabezad@gmail.com>
2022-11-08 06:12:45 -0400

    actualizando

---
+++ modulo-1/leccion-4/condicionales.js
@@ -0,0 +1,10 @@
+// IF \
+const edad = 17;
+
+if (edad >= 18) {
+  console.log("Eres mayor de edad");
+} else if (edad < 18) {
+  console.log("Eres menor de edad");
+} else {
+  console.log("No reconozco tu edad");
+}
