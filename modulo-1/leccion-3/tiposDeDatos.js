af0dd55486c8c879e8c21f2ff6e3812cebefa1bb
Ursulo Cabeza <ursuloacabezad@gmail.com>
2022-11-07 20:26:55 -0400

    agregando leccion-3

---
+++ modulo-1/leccion-3/tiposDeDatos.js
@@ -0,0 +1,15 @@
+// TIPOS DE DATOS - Primitivos - Objetos
+
+// string - number - boolean - null - undefined
+
+const nombre = "Ursulo";
+const edad = 20;
+const vivo = true;
+const millonario = null;
+const robot = undefined;
+
+// OBJETOS
+
+const persona = { nombre: "Ursulo", edad: 20 };
+
+const arr = ["ursulo", "daniel", "allison"];
